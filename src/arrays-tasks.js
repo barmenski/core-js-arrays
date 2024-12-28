2e729da8e448d61ff7caffc09e54824ce5557d9a
barmenski <1kiloom@gmail.com>
2024-12-29 01:08:04 +0300

    feat: 8 tasks pass

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -73,8 +73,8 @@ function sumArrays(arr1, arr2) {
  *    findElement(['Array', 'Number', 'string'], 'Date') => -1
  *    findElement([0, 1, 2, 3, 4, 5], 5) => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  return arr.indexOf(value);
 }
 
 /**
@@ -91,8 +91,9 @@ function findElement(/* arr, value */) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  const res = arr.filter((elem) => elem === item);
+  return res.length;
 }
 
 /**
@@ -107,8 +108,18 @@ function findAllOccurrences(/* arr, item */) {
  *    removeFalsyValues([ 1, 2, 3, 4, 5, 'false' ]) => [ 1, 2, 3, 4, 5, 'false' ]
  *    removeFalsyValues([ false, 0, NaN, '', undefined ]) => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues(arr) {
+  const res = arr.filter(
+    (elem) =>
+      elem === false ||
+      elem === null ||
+      elem === 0 ||
+      elem === '' ||
+      elem === undefined ||
+      elem === null ||
+      Number.isNaN(elem)
+  );
+  return res;
 }
 
 /**
